8eda4b6ca92d77ce5e1a1f41d030e7e6b652fe5d
Carlos Henrique <henriquesbral@gmail.com>
2025-04-15 21:57:15 -0300

    Ultimos ajustes no conversor de moedas

--- ConversorMoedas/ConverterMoeda.cs
+++ ConversorMoedas/ConverterMoeda.cs
@@ -8,29 +8,57 @@ namespace ConversorMoedas
 {
     public class ConverterMoeda
     {
-        private double Dolar = 5.8605;
-        private double Euro = 6.6225;
-        private double BitCoinDolar = 85906.10;
-        private double BitCoinReais = 504074.64;
+        private double Dolar = 5.8859;
+        private double Euro = 6.6656;
+        private double BitCoinDolar = 83441.83;
+        private double BitCoinReais = 491124.38;
 
         public void Menu()
         {
             string menu = $"Bem vindo ao conversor de moedas para reais!" +
                            "\n As moedas disponíveis para conversão são:" +
-                           "\n - Dólar" +
-                           "\n - Euro" +
-                           "\n - BitCoin em Dólar" +
-                           "\n - BitCoin em Reais" +
-                           "\n" +
-                           "\nSelecione a moeda que deseja converter: ";
+                           "\n1 - Dólar" +
+                           "\n2 - Euro" +
+                           "\n3 - BitCoin em Dólar" +
+                           "\n4 - BitCoin em Reais" +
+                           "\nValores das moedas atualizados hoje ás 21:30" +
+                           "\n";
             Console.WriteLine(menu);            
         }
 
-        public double RetornaValorConvertido(double n)
+        public string OpcaoSelecionada(int op, double valor)
         {
-            double valor = 0;
+            var opcao = op;
+            string retorno = "";
+            switch (opcao)
+            {
+                case 1:
+                    retorno = $"O valor de R$ {valor} convertido em Dólar é: R$ {RetornaValorConvertido(valor, Dolar)}";
+                    break;
+                case 2:
+                    retorno = $"O valor de R$ {valor} convertido em Euro é: R$ {RetornaValorConvertido(valor, Euro)}";
+                    break;
+                case 3:
+                    retorno = $"O valor de R$ {valor} convertido em BitCoin em dólar é: R$ {RetornaValorConvertido(valor, BitCoinDolar)}";    
+                    break;
+                case 4:
+                    retorno = $"O valor de R$ {valor} convertido em BitCoin em reais é: R$ {RetornaValorConvertido(valor, BitCoinReais)}";
+                    break;
+                default:
+                    retorno = "Opção inválida, por gentileza verifique novamente as opções";
+                    break;
+            }
 
-            return valor;
+            return retorno;
+        }
+
+        public decimal RetornaValorConvertido(double valor, double moeda)
+        {
+            decimal valorConvertido = 0;
+
+            valorConvertido = Convert.ToDecimal(valor * moeda);
+
+            return valorConvertido;
         }
     }
 }
--- ConversorMoedas/Program.cs
+++ ConversorMoedas/Program.cs
@@ -1,12 +1,33 @@
-﻿
-using ConversorMoedas;
+﻿using ConversorMoedas;
 
 ConverterMoeda converter = new ConverterMoeda();
 
+List<int> opcoes = new List<int>() {1,2,3,4};
 bool whi = true;
 while (whi)
 {
+    Console.Clear();
     converter.Menu();
-    var moeda = Console.ReadLine();
-    converter.RetornaValorConvertido();
+    Console.WriteLine("\nSelecione a moeda que deseja converter ou 0 para sair: ");
+    var moedaSelecionada = Convert.ToInt32(Console.ReadLine());
+
+    if (moedaSelecionada == 0)
+    {
+        Console.WriteLine("Obrigado por utilizar o nosso conversor de moedas !");
+        whi = false;
+    }
+    else if (!opcoes.Contains(moedaSelecionada))
+    {
+        throw new Exception("Opção inválida por gentileza verifique novamente !");
+    }
+    else
+    {
+        Console.WriteLine($"Insira o valor abaixo para conversão: ");
+        var valorAConverter = Convert.ToDouble(Console.ReadLine());
+
+        var retorno = converter.OpcaoSelecionada(moedaSelecionada, valorAConverter);
+        Console.WriteLine(retorno);
+        Console.WriteLine("\nPressione enter para continuar !");
+        var enter = Console.ReadLine();
+    }
 }
