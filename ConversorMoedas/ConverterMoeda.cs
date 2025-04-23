96b8cdcb563cd46b7f2a6d65db05f4a6ba9132d6
Carlos Henrique <henriquesbral@gmail.com>
2025-04-22 22:52:45 -0300

    Aplicando conceitos de encapsulamento no projeto ConversorMoedas

--- ConversorMoedas/ConverterMoeda.cs
+++ ConversorMoedas/ConverterMoeda.cs
@@ -8,38 +8,73 @@ namespace ConversorMoedas
 {
     public class ConverterMoeda
     {
-        private double Dolar = 5.8859;
+        private double Dolar = 5.73;
         private double Euro = 6.6656;
         private double BitCoinDolar = 83441.83;
         private double BitCoinReais = 491124.38;
 
-        public void Menu()
+        public void LoopingConversao()
+        {
+            List<int> opcoes = new List<int>() { 1, 2, 3, 4 };
+            bool whi = true;
+            while (whi)
+            {
+                Console.Clear();
+                Menu();
+                Console.WriteLine("\nSelecione a moeda que deseja converter ou 0 para sair: ");
+                var moedaSelecionada = Convert.ToInt32(Console.ReadLine());
+
+                if (moedaSelecionada == 0)
+                {
+                    Console.WriteLine("Obrigado por utilizar o nosso conversor de moedas !");
+                    whi = false;
+                }
+                else if (!opcoes.Contains(moedaSelecionada))
+                {
+                    Console.WriteLine("Opção inválida por gentileza verifique novamente !");
+                    Console.WriteLine("\nPressione enter para continuar !");
+                    var enter = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Insira o valor abaixo para conversão: ");
+                    var valorAConverter = Convert.ToDouble(Console.ReadLine());
+
+                    var retorno = OpcaoSelecionada(moedaSelecionada, valorAConverter);
+                    Console.WriteLine(retorno);
+                    Console.WriteLine("\nPressione enter para continuar !");
+                    var enter = Console.ReadLine();
+                }
+            }
+        }
+
+        private void Menu()
         {
             string menu = $"Bem vindo ao conversor de moedas para reais!" +
                            "\n As moedas disponíveis para conversão são:" +
-                           "\n1 - Dólar" +
-                           "\n2 - Euro" +
-                           "\n3 - BitCoin em Dólar" +
-                           "\n4 - BitCoin em Reais" +
-                           "\nValores das moedas atualizados hoje ás 21:30" +
+                           "\n 1 - Dólar = R$ 5,72" +
+                           "\n 2 - Euro = R$ 6,52" +
+                           "\n 3 - BitCoin em Dólar = US$ 93017,58" +
+                           "\n 4 - BitCoin em Reais = R$ 532.617,69" +
+                           "\n Valores das moedas atualizadas 22/04/2025 ás 21:30" +
                            "\n";
             Console.WriteLine(menu);            
         }
 
-        public string OpcaoSelecionada(int op, double valor)
+        private string OpcaoSelecionada(int op, double valor)
         {
             var opcao = op;
             string retorno = "";
             switch (opcao)
             {
                 case 1:
-                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em Dólar é: R$ {RetornaValorConvertido(valor, Dolar).ToString("N2")}";
+                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em Dólar é: US$ {RetornaValorConvertido(valor, Dolar).ToString("N2")}";
                     break;
                 case 2:
-                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em Euro é: R$ {RetornaValorConvertido(valor, Euro).ToString("N2")}";
+                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em Euro é: € {RetornaValorConvertido(valor, Euro).ToString("N2")}";
                     break;
                 case 3:
-                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em BitCoin em dólar é: R$ {RetornaValorConvertido(valor, BitCoinDolar).ToString("N2")}";    
+                    retorno = $"O valor de R$ {valor.ToString("N2")} convertido em BitCoin em dólar é: US$ {RetornaValorConvertido(valor, BitCoinDolar).ToString("N2")}";    
                     break;
                 case 4:
                     retorno = $"O valor de R$ {valor} convertido em BitCoin em reais é: R$ {RetornaValorConvertido(valor, BitCoinReais).ToString("N2")}";
@@ -52,11 +87,11 @@ namespace ConversorMoedas
             return retorno;
         }
 
-        public decimal RetornaValorConvertido(double valor, double moeda)
+        private decimal RetornaValorConvertido(double valor, double moeda)
         {
             decimal valorConvertido = 0;
 
-            valorConvertido = Convert.ToDecimal(valor * moeda);
+            valorConvertido = Convert.ToDecimal(valor / moeda);
 
             return valorConvertido;
         }
--- ConversorMoedas/Program.cs
+++ ConversorMoedas/Program.cs
@@ -1,34 +1,4 @@
 ﻿using ConversorMoedas;
 
 ConverterMoeda converter = new ConverterMoeda();
-
-List<int> opcoes = new List<int>() {1,2,3,4};
-bool whi = true;
-while (whi)
-{
-    Console.Clear();
-    converter.Menu();
-    Console.WriteLine("\nSelecione a moeda que deseja converter ou 0 para sair: ");
-    var moedaSelecionada = Convert.ToInt32(Console.ReadLine());
-
-    if (moedaSelecionada == 0)
-    {
-        Console.WriteLine("Obrigado por utilizar o nosso conversor de moedas !");
-        whi = false;
-    }
-    else if (!opcoes.Contains(moedaSelecionada))
-    {
-        Console.WriteLine("Opção inválida por gentileza verifique novamente !");
-        continue;
-    }
-    else
-    {
-        Console.WriteLine($"Insira o valor abaixo para conversão: ");
-        var valorAConverter = Convert.ToDouble(Console.ReadLine());
-
-        var retorno = converter.OpcaoSelecionada(moedaSelecionada, valorAConverter);
-        Console.WriteLine(retorno);
-        Console.WriteLine("\nPressione enter para continuar !");
-        var enter = Console.ReadLine();
-    }
-}
+converter.LoopingConversao();
